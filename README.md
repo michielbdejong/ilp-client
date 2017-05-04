6b46a8818cba1c03fdeead6ba656b20c7afc48bd
Michiel de Jong <michiel@ripple.com>
2017-05-04 13:54:09 +0200

    updateStats action

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Common parts between [micmic](https://github.com/michielbdejong/micmic) and [con
 git clone https://github.com/michielbdejong/ilp-client
 cd ilp-client
 npm install
-node src/index.js ./stats.js ./credentials.js
+node src/index.js stats.json creds.json updateStats
 ```
--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 const fs = require('fs')
 const getHostInfo = require('./hostInfo')
 
-if (process.argv.length < 3) {
-  throw new Error('Usage: node src/index.js statistics.json credentials.json')
+if (process.argv.length < 5) {
+  throw new Error('Usage: node src/index.js statistics.json credentials.json updateStats')
 }
 
 const statsFileName = process.argv[2]
 const credsFileName = process.argv[3]
+const action = process.argv[4]
 
 const stats = JSON.parse(fs.readFileSync(statsFileName))
 const creds = JSON.parse(fs.readFileSync(credsFileName))
 console.log(stats, creds)
 
-Promise.all(Object.keys(stats.hosts).map(async function(hostname) {
-  stats.hosts[hostname] = await getHostInfo(hostname, stats.hosts[hostname])
-})).then(() => {
-  fs.writeFileSync(statsFileName, JSON.stringify(stats, null, 2))
-})
+switch (action) {
+case 'updateStats':
+  Promise.all(Object.keys(stats.hosts).map(async function(hostname) {
+    stats.hosts[hostname] = await getHostInfo(hostname, stats.hosts[hostname])
+  })).then(() => {
+    fs.writeFileSync(statsFileName, JSON.stringify(stats, null, 2))
+  })
+  break
+}
