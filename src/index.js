7a7f289fb7a0bc45c106d49352f9ecee3453dd9f
Michiel de Jong <michiel@ripple.com>
2017-05-05 16:51:09 +0200

    Minimal interface for connector.land koa implementation

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 const fs = require('fs')
-const getHostInfo = require('./hostInfo')
+const getHostInfo = require('./lib/hostInfo')
+const handleWebFinger = require('./lib/webfinger')
+const handleRpc = require('./lib/rpc')
 
-if (process.argv.length < 5) {
-  throw new Error('Usage: node src/index.js statistics.json credentials.json updateStats')
-}
-
-const statsFileName = process.argv[2]
-const credsFileName = process.argv[3]
-const action = process.argv[4]
-
-const stats = JSON.parse(fs.readFileSync(statsFileName))
-const creds = JSON.parse(fs.readFileSync(credsFileName))
-console.log(stats, creds)
-
-switch (action) {
-case 'updateStats':
-  Promise.all(Object.keys(stats.hosts).map(async function(hostname) {
-    stats.hosts[hostname] = await getHostInfo(hostname, stats.hosts[hostname])
-  })).then(() => {
-    fs.writeFileSync(statsFileName, JSON.stringify(stats, null, 2))
-  })
-  break
+module.exports = function(statsFileName, credsFileName, hostname) {
+  const creds = JSON.parse(fs.readFileSync(credsFileName))
+  return {
+    stats: JSON.parse(fs.readFileSync(statsFileName)),
+    testHost: async function(hostname) {
+      this.stats.hosts[hostname] = await getHostInfo(hostname, this.stats.hosts[hostname] || {})
+      fs.writeFile(statsFileName, JSON.stringify(this.stats, null, 2))
+    },
+    handleWebFinger,
+    handleRpc
+  }
 }
