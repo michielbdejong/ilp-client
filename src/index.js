84ad1283e5b97ea1b925dfa204679d026dc2e758
Michiel de Jong <michiel@ripple.com>
2017-05-08 16:19:47 +0200

    Version 2.2.0: expose .testAll

--- src/index.js
+++ src/index.js
@@ -7,9 +7,27 @@ module.exports = function(statsFileName, credsFileName, hostname) {
   const creds = JSON.parse(fs.readFileSync(credsFileName))
   return {
     stats: JSON.parse(fs.readFileSync(statsFileName)),
-    testHost: async function(hostname) {
+    writeStats: async function() {
+      await new Promise((resolve, reject) => {
+        fs.writeFile(statsFileName, JSON.stringify(this.stats, null, 2), function(err) {
+          if (err) reject(err)
+          resolve()
+        })
+      })
+    },
+    testAll: async function() {
+      const promises = []
+      for (let hostname of Object.keys(this.stats.hosts)) {
+        promises.push(this.testHost(hostname, false))
+      }
+      await Promise.all(promise)
+      await this.writeStats()
+    },
+    testHost: async function(hostname, writeStats = true) {
       this.stats.hosts[hostname] = await getHostInfo(hostname, this.stats.hosts[hostname] || {})
-      fs.writeFile(statsFileName, JSON.stringify(this.stats, null, 2))
+      if (writeStats) {
+        await this.writeStats()
+      }
     },
     handleWebFinger,
     handleRpc
