4fb8a5a7ca206979e3769b9108aa409c207d6a5f
Michiel de Jong <michiel@ripple.com>
2017-05-09 15:25:54 +0200

    implement webfinger

--- src/index.js
+++ src/index.js
@@ -3,33 +3,41 @@ const getHostInfo = require('./lib/hostInfo')
 const handleWebFinger = require('./lib/webfinger')
 const handleRpc = require('./lib/rpc')
 
-module.exports = function(statsFileName, credsFileName, hostname) {
-  const creds = JSON.parse(fs.readFileSync(credsFileName))
-  return {
-    stats: JSON.parse(fs.readFileSync(statsFileName)),
-    writeStats: async function() {
-      await new Promise((resolve, reject) => {
-        fs.writeFile(statsFileName, JSON.stringify(this.stats, null, 2), function(err) {
-          if (err) reject(err)
-          resolve()
-        })
-      })
-    },
-    testAll: async function() {
-      const promises = []
-      for (let hostname of Object.keys(this.stats.hosts)) {
-        promises.push(this.testHost(hostname, false))
-      }
-      await Promise.all(promise)
-      await this.writeStats()
-    },
-    testHost: async function(hostname, writeStats = true) {
-      this.stats.hosts[hostname] = await getHostInfo(hostname, this.stats.hosts[hostname] || {})
-      if (writeStats) {
-        await this.writeStats()
-      }
-    },
-    handleWebFinger,
-    handleRpc
-  }
+function IlpNode (statsFileName, credsFileName, hostname) {
+  this.stats = JSON.parse(fs.readFileSync(statsFileName))
+  this.creds = JSON.parse(fs.readFileSync(credsFileName))
+  this.hostname = hostname
 }
+
+IlpNode.prototype = {
+  writeStats: async function() {
+    await new Promise((resolve, reject) => {
+      fs.writeFile(statsFileName, JSON.stringify(this.stats, null, 2), function(err) {
+        if (err) reject(err)
+        resolve()
+      })
+    })
+  },
+  testAll: async function() {
+    const promises = []
+    for (let hostname of Object.keys(this.stats.hosts)) {
+      promises.push(this.testHost(hostname, false))
+    }
+    await Promise.all(promise)
+    await this.writeStats()
+  },
+  testHost: async function(hostname, writeStats = true) {
+    this.stats.hosts[hostname] = await getHostInfo(hostname, this.stats.hosts[hostname] || {})
+    if (writeStats) {
+      await this.writeStats()
+    }
+  },
+  handleWebFinger: async function(resource) {
+    return handleWebFinger(resource, this.creds, this.hostname)
+  },
+  handleRpc: async function(params) {
+    return handleRpc(params, this.creds)
+  },
+}
+
+module.exports = IlpNode
--- src/lib/webfinger.js
+++ src/lib/webfinger.js
@@ -1,1 +1,27 @@
-module.exports = () => {}
+module.exports = (resource, creds, hostname) => {
+  if (typeof resource !== 'string') {
+    return
+  }
+  if (resource === `https://${hostname}`) {
+    return {
+      subject: resource,
+      properties: {
+        'https://interledger.org/rel/protocolVersion': 'Compatible: ilp-kit v2.0.0',
+        'https://interledger.org/rel/publicKey': creds.keypair.pub
+      },
+      links:[
+      ]
+    }
+  } else if (resource.startsWith('acct:')) {
+    const parts = resource.substring('acct:'.length).split('@')
+    if (parts[1] === hostname) {
+      return {
+        subject: resource,
+        links: [
+          { rel: 'https://interledger.org/rel/ilpAddress', href: `g.dns.${hostname.split('.').reverse().join('.')}.${parts[0]}` },
+          { rel: 'https://interledger.org/rel/spsp/v2', href: `https://${hostname}/spsp` }
+        ]
+      }
+    }
+  }
+}
