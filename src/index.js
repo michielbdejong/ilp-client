b6bfffcce3610f0f726755236e7bdc4682de4bb8
Michiel de Jong <michiel@ilp.michielbdejong.com>
2017-05-09 14:32:30 +0000

    fixes on connector.land

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const handleWebFinger = require('./lib/webfinger')
 const handleRpc = require('./lib/rpc')
 
 function IlpNode (statsFileName, credsFileName, hostname) {
+  console.log('function IlpNode (', { statsFileName, credsFileName, hostname })
+  this.statsFileName = statsFileName
   this.stats = JSON.parse(fs.readFileSync(statsFileName))
   this.creds = JSON.parse(fs.readFileSync(credsFileName))
   this.hostname = hostname
@@ -12,7 +14,7 @@ function IlpNode (statsFileName, credsFileName, hostname) {
 IlpNode.prototype = {
   writeStats: async function() {
     await new Promise((resolve, reject) => {
-      fs.writeFile(statsFileName, JSON.stringify(this.stats, null, 2), function(err) {
+      fs.writeFile(this.statsFileName, JSON.stringify(this.stats, null, 2), function(err) {
         if (err) reject(err)
         resolve()
       })
@@ -21,13 +23,13 @@ IlpNode.prototype = {
   testAll: async function() {
     const promises = []
     for (let hostname of Object.keys(this.stats.hosts)) {
-      promises.push(this.testHost(hostname, false))
+      promises.push(this.testHost(this.hostname, false))
     }
-    await Promise.all(promise)
+    await Promise.all(promises)
     await this.writeStats()
   },
-  testHost: async function(hostname, writeStats = true) {
-    this.stats.hosts[hostname] = await getHostInfo(hostname, this.stats.hosts[hostname] || {})
+  testHost: async function(testHostname, writeStats = true) {
+    this.stats.hosts[testHostname] = await getHostInfo(testHostname, this.stats.hosts[testHostname] || {})
     if (writeStats) {
       await this.writeStats()
     }
