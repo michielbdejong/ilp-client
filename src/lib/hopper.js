64406d9eac4d3491be76518ebe3c01e95ebb2bff
Michiel de Jong <michiel@ripple.com>
2017-05-16 14:42:14 +0200

    some more Hopper code, now need unit tests

--- src/lib/hopper.js
+++ src/lib/hopper.js
@@ -4,26 +4,79 @@ function Hopper(ilpNode) {
   this.ilpNode = ilpNode
 }
 
-Hopper.prototype.makeTable = function(destination, excludePeer) {
-  let sections = []
+let calcSlope = (start, end) => ((end[1]-start[1]) / (end[0]-start[0]))
+let valueAt = (candidate, pointIn) => {
+  let dOut = (pointIn - candidate.startIn) * candidate.slope
+  return candidate.startOut + dOut
+}
+let  usefulAt = (candidate, point) => (this.valueAt(candidate, point.startIn) > point.startOut)
+
+HopperTable = function(destination, excludePeer) {
+  this.sections = [] // array of objects: { startIn, startOut, slope, peerHost }
+                     // sorted by startIn
+                     // implied first (0, 0) point is omitted
+
   for (let peerHost in this.ilpNode.peers) {
     if (peerHost !== excludePeer && this.ilpNode.peers[peerHost].routes[destination]) {
-      let sectionsCursor = 0
       for (let i=1; i<this.ilpNode.peers[peerHost].routes[destination].length; i++) {
-        let sectionStart = this.ilpNode.peers[peerHost].routes[destination][i-1]
-        let sectionEnd = this.ilpNode.peers[peerHost].routes[destination][i]
-        while (sectionCursor < sections.length) {
-          if (sectionStart[0] > sections[sectionCursor][0]) {
-            sectionCursor++
-          } else {
-            if (sectionStart[1] > sections[sectionCursor]
-          }
-          // compare our section to the one under the cursor.
-          // advance the secton cursor until we find the section with which to do crossover
-          // or part of it (in which case we can use binary search to find the exact crossover.
+        let start = this.ilpNode.peers[peerHost].routes[destination][i-1]
+        let end = this.ilpNode.peers[peerHost].routes[destination][i]
+        let slope = calcSlope(start, end)
+        this.addSection({ startIn: start[0], endIn: end[0], startOut: start[1], slope, peerHost })
+      }
+    }
+  }
+}
+ 
+HopperTable.prototype= {
+  crossover(obj, i) {
+    let startDiff = valueAt(obj, this.sections[i].startIn) - this.sections[i].startOut
+    let relSlope = this.sections[i].slope - obj.slope
+    let crossoverIn = startDiff / relSlope
+    let crossoverOut = valueAt(obj, crossoverIn)
+    let parts = []
+    this.sections.splice(i, 1, {
+      startIn: this.sections[i].startIn,
+      startOut: (startDiff < 0 ? this.sections[i].startOut : obj.startOut),
+      slope: (startDiff < 0 ? this.sections[i].slope : obj.slope),
+      peerHost: (startDiff < 0 ? this.sections[i].peerHost : obj.peerHost)
+    },
+    {
+      startIn: crossoverIn,
+      startOut: crossoverOut,
+      slope: (startDiff < 0 ? obj.slope : this.sections[i].slope),
+      peerHost: (startDiff < 0 ? obj.peerHost : this.sections[i].peerHost)
+    })
+  },
+  addKnee(pointIn) {
+    const knee = {
+      startIn: pointIn,
+      startOut: 0,
+      slope: 0,
+      peerHost: 'to be determined'
+    }
+    for (let i=1; i<this.sections.length; i++) {
+      if (this.sections[i] > pointIn) {
+        a.splice(i-1, 0, knee)
+        return
+      }
+    }
+    this.sections.push(knee)
+  },
+  addSection(obj) {
+    let startKnee = addKnee(obj.startIn)
+    let endKnee = addKnee(obj.endIn)
+    for (let i = startKnee; i < endKnee; i++) {
+      let usefulStart = usefulAt(obj, this.sections[i])
+      let usefulEnd = usefulAt(obj, this.sections[i + 1])
+      if (usefulStart) {
+        if (usefulEnd) {
+           this.sections[i] = obj
+        } else {
+          crossover(obj, i)
         }
-        sections.push([ sectionEnd[0], sectionEnd[1], peerHost ])
-        sectionCursor++
+      } else if (usefulEnd) {
+        crossover(obj, i)
       }
     }
   }
@@ -32,6 +85,14 @@ Hopper.prototype.makeTable = function(destination, excludePeer) {
 Hopper.prototype.makeCurve = function(forPeer, toDestination) {
   let sourceRate = this.ilpNode.peers[forPeer].rate * COMMISSION
   let nextHopRate = this.ilpNode.peers[section[2]].rate
-  return this.makeTable(toDestination, forPeer)
-    .map(section => [section[0] * sourceRate, section[1] * nextHopRate)
+  let hopperTable = new HopperTable(toDestination, forPeer)
+  return hopperTable.sections.map(section => [section[0] * sourceRate, section[1] * nextHopRate ])
+}
+
+module.exports = {
+  Hopper,
+  HopperTable,
+  calcSlope,
+  valueAt,
+  usefulAt
 }
