6acd553dff6a2fae84d1a03423dfea851a6f5214
Michiel de Jong <michiel@ripple.com>
2017-07-27 19:20:01 +0200

    addresses to upper case

--- src/eth-tester.js
+++ src/eth-tester.js
@@ -1,10 +1,10 @@
 console.log('Instantiating xrp tester',
   process.env.PROVIDER,
-  process.env.ADDRESS1,
+  process.env.ADDRESS1.toUpperCase(),
   process.env.SECRET1,
-  process.env.ADDRESS2,
+  process.env.ADDRESS2.toUpperCase(),
   process.env.SECRET2,
-  process.env.CONNECTOR
+  process.env.CONNECTOR.toUpperCase()
 )
 const pay = require('./test-payment')
 const Plugin = require('ilp-plugin-ethereum')
@@ -12,18 +12,18 @@ const Plugin = require('ilp-plugin-ethereum')
 const prefix = 'test.crypto.eth.rinkeby.'
 const plugin1 = new Plugin({
   provider: process.env.PROVIDER,
-  address: process.env.ADDRESS1,
+  address: process.env.ADDRESS1.toUpperCase(),
   secret: process.env.SECRET1,
-  contract: '0x8b3fbd781096b51e68448c6e5b53b240f663199f',
+  contract: '0x8b3fbd781096b51e68448c6e5b53b240f663199f'.toUpperCase(),
   prefix
 })
 const plugin2 = new Plugin({
   provider: process.env.PROVIDER,
-  address: process.env.ADDRESS2,
+  address: process.env.ADDRESS2.toUpperCase(),
   secret: process.env.SECRET2,
-  contract: '0x8b3fbd781096b51e68448c6e5b53b240f663199f',
+  contract: '0x8b3fbd781096b51e68448c6e5b53b240f663199f'.toUpperCase(),
   prefix
 })
 
 // pay from wallet 1 to wallet 2:
-pay(plugin1, plugin2, process.env.CONNECTOR)
+pay(plugin1, plugin2, process.env.CONNECTOR.toUpperCase())
