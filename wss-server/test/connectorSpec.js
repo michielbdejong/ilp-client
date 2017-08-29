42487e03be5428feb1888f9f6a159660a1f949a6
Michiel de Jong <michiel@ripple.com>
2017-08-29 13:19:45 +0200

    two clients

--- wss-server/test/connectorSpec.js
+++ wss-server/test/connectorSpec.js
@@ -1,4 +1,8 @@
 const Connector = require('../connector')
+const Peer = require('../peer')
+const Quoter = require('../peer')
+const Forwarder = require('../peer')
+
 const assert = require('chai').assert
 const WebSocket = require('ws');
 
@@ -11,18 +15,42 @@ describe('Connector', () => {
     return this.connector.close()
   })
 
-  describe('one client', () => {
-    beforeEach(function () {
-      this.client1 = new WebSocket('ws://localhost:8000/path', {
+  describe('two clients', () => {
+    beforeEach(function (done) {
+      let doneOne = false
+      this.ws1 = new WebSocket('ws://localhost:8000/path', {
         perMessageDeflate: false
+      })
+      this.ws1.on('open', function open() {
+        console.log('ws1 open')
+        this.quoter1 = new Quoter()
+        this.peers1 = {}
+        this.forwarder1 = new Forwarder(this.quoter1, this.peers1)
+        console.log('creating client peer')
+        this.peer1 = new Peer('peer1', 0, this.ws1, this.quoter1, this.forwarder1)
+        if (doneOne) { done() } else { doneOne = true }
+      })
+      this.ws2 = new WebSocket('ws://localhost:8000/path', {
+        perMessageDeflate: false
+      })
+      this.ws2.on('open', function open() {
+        this.quoter2 = new Quoter()
+        this.peers2 = {}
+        this.forwarder2 = new Forwarder(this.quoter2, this.peers2)
+        this.peer2 = new Peer('peer2', 0, this.ws2, this.quoter2, this.forwarder2)
+        if (doneOne) { done() } else { doneOne = true }
       })
     })
     afterEach(function () {
       this.client1.close()
+      this.client2.close()
     })
-    it('should connect', function (done) {
-      this.client1.on('open', function open() {
-        done()
+    it('should respond to quote', function () {
+      return this.peer1.unpaid('ilp', IlpPacket.serializeIlqpLiquidityRequest({
+        destinationAccount: 'example.nexus.bob',
+        destinationHoldDuration: 3000
+      })).then(result => {
+        console.log(result)
       })
     })
   })
