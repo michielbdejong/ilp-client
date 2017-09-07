228968c5485992c198cc298c063caa983effb8bf
Michiel de Jong <michiel@ripple.com>
2017-09-07 11:40:30 +0200

    correct server account

--- config/server.js
+++ config/server.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   eth: {
     secret: 'xidaequeequuu4xah8Ohnoo1Aesumiech6tiay1h',
-    address: '0x' + '8b3fbd781096b51e68448c6e5b53b240f663199f'.toUpperCase(),
+    address: '0x' + 'fa5b9836c46b6559be750b2f3c12657081fab858'.toUpperCase(),
     provider: 'http://localhost:8545',
     contract: '0x8B3FBD781096B51E68448C6E5B53B240F663199F',
     prefix: 'test.crypto.eth.rinkeby.'
