81c8175658fd89b83ddd2a34157b95600087b20c
Michiel de Jong <michiel@ripple.com>
2017-09-08 16:59:55 +0200

    debugs

--- scripts/flood.js
+++ scripts/flood.js
@@ -73,8 +73,8 @@ flooder.open().then(() => {
   startTime = new Date().getTime()
   return flooder.flood(NUM, from, to)
 }).then(() => {
-  // const endTime = new Date().getTime()
-  // console.log(NUM + ' transfers took ' + (endTime - startTime) + 'ms, that is '  + (1000 * NUM / (endTime - startTime)) + ' payments per second.')
+  const endTime = new Date().getTime()
+  console.log(NUM + ' transfers took ' + (endTime - startTime) + 'ms, that is ' + (1000 * NUM / (endTime - startTime)) + ' payments per second.')
   // console.log(Object.keys(flooder.client1.peer.clp.transfersSent).length) -> 0
   flooder.close()
 }, err => {
