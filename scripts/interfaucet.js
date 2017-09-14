8c032483a2f3a802005a85b4e366b80c71fa2c73
Michiel de Jong <michiel@ripple.com>
2017-09-14 10:38:55 +0200

    missing part of interfaucet script, ready for testing on server

--- scripts/interfaucet.js
+++ scripts/interfaucet.js
@@ -1,8 +1,11 @@
-const ilpPacket = require('ilp-packet')
+const IlpNode = require('../src/index')
+const IlpPacket = require('ilp-packet')
 const getLetsEncryptServers = require('../src/letsencrypt')
 
+const client = new IlpNode(require('../config/client1'))
+
+client.start().then(() => {
 getLetsEncryptServers('interfaucet.michielbdejong.com', (req, res) => {
-  // '', 'fund', 'deadbeef'
   const parts = req.url.split('/')
   console.log('interfaucet request!', parts)
   const iprBuf = Buffer.from(parts[2], 'hex')
@@ -11,4 +14,13 @@ getLetsEncryptServers('interfaucet.michielbdejong.com', (req, res) => {
     packet: IlpPacket.deserializeIlpPayment(iprBuf.slice(1, iprBuf.length - 8)),
     condition: iprBuf.slice(-8)
   }
+  client.getPeer('clp').interledgerPayment({
+    amount: parsed.packet.amount,
+    executionCondition: parsed.condition,
+    expiresAt: new Date(new Date().getTime() + 3600 * 1000)
+  }, parsed.packet).then(() => {
+    res.end('SENT!')
+  }, err => {
+    res.end(JSON.stringify(err))
+  })
 })
